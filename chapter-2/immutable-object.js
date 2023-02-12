aaaa829bab6b0461b8ff0b379c3763a20b264cff
Josh McHugh <joshua.mchugh01@gmail.com>
2023-02-12 00:01:20 -0500

    Added additional comments to explain how JavaScript freeze runtime error

--- chapter-2/immutable-object.js
+++ chapter-2/immutable-object.js
@@ -1,5 +1,6 @@
 //BROKEN CHANGE
 //This example shows how to make use the Object.freeze function with const to make immutable objects where the object refernce and interal values are immutable. It is a shallow freeze and only effect thte first level.
+//The code below fails when sam.age is assigned to 3. A runtime error occurs with the message 'TypeError: Cannot assign to read only property 'age' of object '#<Object>'
 'use strict';
 const sam = Object.freeze({ first: 'Sam', age: 2 });
 
