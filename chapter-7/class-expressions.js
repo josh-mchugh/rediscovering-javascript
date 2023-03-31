24d7104d7177e970b1332b7ec1243207c0097db8
Josh McHugh <joshua.mchugh01@gmail.com>
2023-03-31 00:11:03 -0400

    Add additional code comments

--- chapter-7/class-expressions.js
+++ chapter-7/class-expressions.js
@@ -1,5 +1,5 @@
 'use-strict';
-//
+//Class expressions are useful to create dynamic classes at runtime. Class expressions can be used to create a class factory function that will create classes.
 const createClass = function(...fields) {
     return class {
 	constructor(...values) {
@@ -8,11 +8,12 @@ const createClass = function(...fields) {
     };
 };
 
+//The classes created from the class factory create anonymous classes which can be assigned a name.
 const Book = createClass('title', 'subtitle', 'pages');
 console.log(Book);
-
 const Actor = createClass('firstName', 'lastName', 'yearStarted');
 console.log(Actor);
 
+//Once we obtained a reference to classes created using the class expressions, we can use them like classes defined using the class statement.
 const fisher = new Actor('Carrie', 'Fisher', 1969);
 console.log(fisher);
